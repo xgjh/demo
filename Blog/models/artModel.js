18fe03974d7a43c53f39939f8eadd2fc3c3fa2ef
xgjh <15730299469@163.com>
2018-11-18 21:10:01 +0800

    delete

--- Blog/models/artModel.js
+++
@@ -1,6 +0,0 @@
-var mongoose=require('mongoose');
-//引入表结构对象
-var artSchema=require('../schemas/artShema');
-//创建一个可以操作表结构的数据模型对象
-var artModel=mongoose.model('artModel',artSchema);
-module.exports=artModel;
